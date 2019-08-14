e9df2cd7400b7d9528f9aafa434375f7d22b66a3
kaycloud <jiangyk55@gmail.com>
2019-08-14 21:33:10 +0800

    feature:初始化logger

--- app/src/main/java/com/kaycloud/frost/FrostApp.kt
+++ app/src/main/java/com/kaycloud/frost/FrostApp.kt
@@ -1,6 +1,8 @@
 package com.kaycloud.frost
 
 import android.app.Application
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
 
 /**
  * Created by kaycloud on 2019-07-17
@@ -9,6 +11,8 @@ class FrostApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Logger.addLogAdapter(AndroidLogAdapter())
     }
 
 
