8a552df56f65aa30638b3ee1d23e53a14ae08b20
kaycloud <jiangyk55@gmail.com>
2019-11-24 18:01:35 +0800

    feature：集成Bugly

--- app/src/main/java/com/kaycloud/frost/Constants.kt
+++ app/src/main/java/com/kaycloud/frost/Constants.kt
@@ -8,6 +8,7 @@ const val GANK_DATA_FILENAME = "gank.json"
 
 const val WALLHEAVEN_APIKEY = "LjWrHrZv6N4KIzOgbS9ZbHu1WeoUHS0G"
 
+const val BUGLY_APPID = "c33952f20b"
 
 object DatabaseConstant {
 
--- app/src/main/java/com/kaycloud/frost/FrostApplication.kt
+++ app/src/main/java/com/kaycloud/frost/FrostApplication.kt
@@ -4,16 +4,18 @@ import android.app.Application
 import com.facebook.stetho.Stetho
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
+import com.tencent.bugly.crashreport.CrashReport
 
 /**
  * Created by kaycloud on 2019-07-17
  */
-class FrostApp : Application() {
+class FrostApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         Logger.addLogAdapter(AndroidLogAdapter())
         Stetho.initializeWithDefaults(this)
+        CrashReport.initCrashReport(applicationContext, BUGLY_APPID, true)
     }
 }
